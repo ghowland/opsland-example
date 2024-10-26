09fc4015f2ad7750bafc7ecf00073433c4d8a05e
Geoffrey Howland <geoff@gmail.com>
2024-10-27 01:50:52 +0700

    Basic login works, no auth check yet

--- logic/command_go.py
+++ logic/command_go.py
@@ -10,16 +10,38 @@ MTR_PATH = '/usr/local/bin/mtr'
 
 
 def Site_Login(config):
+  """User performs login action on login page"""
+  username = ''
+
+  if config.input:
+    # We get the user request input, and the user record, which has the password
+    # Verify the password is correct, and give them a token, which we will use to verify auth
+
+    if config.input.get('matched', None):
+      username = config.input['matched'].get('username', '')
+
+      if config.input['request']['password'] == config.input['matched']['password']:
+        result = {'username': config.input['request']['username'], 'token': utility.GetUUID(), 'authed': True}
+
+        LOG.debug(f'\n\nCrud Test: {config.input}')
+        LOG.debug(f'Result: {result}\n\n')
+
+        return result
+
+  # We didnt complete login, so yield no data
+  failed_result = {'username': username, 'token': '', 'authed': False}
+  return failed_result
+
+
+def Site_VerifyAuth(config):
   """"""
   if config.input:
-    result = {'username': 'fakenamehere'}
+    # We should have our Auth information from the Site_Login(), so now we just verify the user and token information
 
-    # LOG.debug(f'Crud Test: {config.input}')
+    result = {'authed': False}
 
-    # output = dict(config.input['request']['data'])
-    # output['_valid'] = True
-    # output['_error'] = None
-    # output['_errors'] = {}
+    LOG.debug(f'Crud Test: {config.input}')
+    LOG.debug(f'Result: {result}')
 
     return result
 
@@ -46,97 +68,97 @@ def Crud_User(config):
 
 
 def Transform2x(config):
-	""""""
-	if config.input:
-		result = {}
+  """"""
+  if config.input:
+    result = {}
 
-		for key, value in config.input['mtr'].items():
-			try:
-				if type(value) in (float, int):
-					result[key] = value * 2
-			except Exception as e:
-				pass
+    for key, value in config.input['mtr'].items():
+      try:
+        if type(value) in (float, int):
+          result[key] = value * 2
+      except Exception as e:
+        pass
 
-		return result
-	
-	else:
-		return None
+    return result
+  
+  else:
+    return None
 
 
 def Deep(config):
-	""""""
-	data = {
-		'more': {
-			'less': [5, 10, 15, 20]
-		},
-		'other': {
-			'deeper': {
-				'test': [5, 10, 99]
-			},
-			'again': {
-				'test': [35, 50, 199]
-			},
-		}
-	}
+  """"""
+  data = {
+    'more': {
+      'less': [5, 10, 15, 20]
+    },
+    'other': {
+      'deeper': {
+        'test': [5, 10, 99]
+      },
+      'again': {
+        'test': [35, 50, 199]
+      },
+    }
+  }
 
-	return data
+  return data
 
 
 def Go(config):
-	""""""
-	data_dict = {
-		'000': {'first': 'Super', 'second': 'Extreme!'},
-		'001': {'first': 'Next', 'second': 'Double'},	
-	}
+  """"""
+  data_dict = {
+    '000': {'first': 'Super', 'second': 'Extreme!'},
+    '001': {'first': 'Next', 'second': 'Double'},  
+  }
 
-	data_list = [
-		{'id': 99, 'first': 'Super', 'second': 'Extreme!'},
-		{'id': 105, 'first': 'Next', 'second': 'Double'},	
-	]
+  data_list = [
+    {'id': 99, 'first': 'Super', 'second': 'Extreme!'},
+    {'id': 105, 'first': 'Next', 'second': 'Double'},  
+  ]
 
-	target = '8.8.8.8'
+  target = '8.8.8.8'
 
-	cmd = f'{MTR_PATH} -r -c1 {target}'
-	(status, output, error) = utility.ExecuteCommand(cmd)
+  cmd = f'{MTR_PATH} -r -c1 {target}'
+  (status, output, error) = utility.ExecuteCommand(cmd)
 
-	mtr = ParseMTR(output, target)
+  mtr = ParseMTR(output, target)
 
-	last_hop = mtr[-1]
+  last_hop = mtr[-1]
 
-	data = {
-		'result': 'Gogogo!', 'data_dict':data_dict, 'data_list':data_list, 'mtr': mtr, 'mtr_last': last_hop, 'hop_count': len(mtr),
-	}
-	return data
+  data = {
+    'result': 'Gogogo!', 'data_dict':data_dict, 'data_list':data_list, 'mtr': mtr, 'mtr_last': last_hop, 'hop_count': len(mtr),
+  }
+  return data
 
 
 def ParseMTR(output, target):
-	""""""
-	hops = []
+  """"""
+  hops = []
 
-	lines = output.split('\n')
+  lines = output.split('\n')
 
-	for line in lines[2:]:
-		while '  ' in line:
-			line = line.replace('  ', ' ')
+  for line in lines[2:]:
+    while '  ' in line:
+      line = line.replace('  ', ' ')
 
-		parts = line.split(' ')
+    parts = line.split(' ')
 
-		if len(parts) > 1:
-			hop = {
-				'index': int(parts[1].replace('.|--', '')),
-				'ip': parts[2],
-				'loss': float(parts[3].replace('%', '')),
-				'sent': int(parts[4]),
-				'last': float(parts[5]),
-				'average': float(parts[6]),
-				'best': float(parts[7]),
-				'worst': float(parts[8]),
-				'stddev': float(parts[9]),
-				'target': target,	#TODO: This shouldnt be in every hop, but putting it in for now because of the table_list key specifier for link
-			}
+    if len(parts) > 1:
+      hop = {
+        'index': int(parts[1].replace('.|--', '')),
+        'ip': parts[2],
+        'loss': float(parts[3].replace('%', '')),
+        'sent': int(parts[4]),
+        'last': float(parts[5]),
+        'average': float(parts[6]),
+        'best': float(parts[7]),
+        'worst': float(parts[8]),
+        'stddev': float(parts[9]),
+        'target': target,  #TODO: This shouldnt be in every hop, but putting it in for now because of the table_list key specifier for link
+      }
 
-			hops.append(hop)
+      hops.append(hop)
 
-	return hops
+  return hops
 
 
